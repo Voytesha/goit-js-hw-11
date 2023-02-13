166c944b73e805b7f2d0049392972b3d1cc024f9
Voytesha <elflowers.info@gmail.com>
2023-02-13 23:23:33 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 import axios from 'axios';
 import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 
 
